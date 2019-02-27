301b129a493e749795c2f6d9f0488e7761f8f8c8
ohhoayu <32924181+ohhoayu@users.noreply.github.com>
2019-02-27 23:37:15 +0800

    Add files via upload
    a new way to deal with <iframe> in selenium

---
+++ selenium2.py
@@ -0,0 +1,27 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+#文件命名注意不要选selenium,会和模块名重合，导致无法正确导入selenium包
+driver = webdriver.Firefox()
+#是www.douban.com中嵌入的html页面，直接使用其中的src导向的链接不正确
+driver.get("https://www.douban.com/")
+
+# 模拟点击登录
+driver.switch_to.frame(0)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/ul[1]/li[2]").click()
+
+driver.find_element_by_name("username").send_keys("18636141566")
+driver.find_element_by_name("password").send_keys("5164390707")
+
+driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[5]/a").click()
+# 等待20秒
+time.sleep(10)
+
+driver.switch_to.default_content()
+# 生成登陆后快照
+driver.save_screenshot(u"douban.png")
+
+driver.quit()
+
